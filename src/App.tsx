a88a5b591848816091c3a9da9e44149972656728
Tianjian Wang <wangtjwork@gmail.com>
2024-07-14 09:47:09 -0700

    Use Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useRef, useState } from 'react'
-import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css'
 import { generateSingleNoteXml, singleNoteXmlDoc } from './utils/musicXMLUtils';
 import { TREBLE_CLEF_NOTES_SCIENTIFIC } from './utils/musicNotesConfig';
 import SingleNoteSheet from './components/SingleNoteSheet';
+import { AppBar, Box, Button, CssBaseline, Stack, Toolbar, Typography } from '@mui/material';
 
 function App() {
   const [note, setNote] = useState<string>('');
@@ -18,10 +22,21 @@ function App() {
 
   return (
     <>
-      <h1>五线谱读音名练习</h1>
-      <button onClick={onStartClick}>{note == '' ? '开始' : '下一题'}</button>
-      {xmlDoc != null ? <SingleNoteSheet xmlDoc={xmlDoc} /> : null}
-      <div>答案：{note}</div>
+      <CssBaseline />
+      <Box flexDirection={'column'} alignItems={'stretch'} textAlign={'center'}>
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              五线谱读音名练习
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Stack flexGrow={1} alignItems={'center'} marginTop={5}>
+          <Button sx={{ width: 'fit-content' }} variant='contained' onClick={onStartClick}>{note == '' ? '开始' : '下一题'}</Button>
+          {xmlDoc != null ? <SingleNoteSheet xmlDoc={xmlDoc} /> : null}
+          <div>答案：{note}</div>
+        </Stack>
+      </Box>
     </>
   )
 }
