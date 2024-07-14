8e3132378f9d3f3642db6b046c26e648c7e53488
Tianjian Wang <wangtjwork@gmail.com>
2024-07-14 13:24:34 -0700

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/3_mins_musical_societies/sw.js', { scope: '/3_mins_musical_societies/' })})}
