701259f9bab8c767fbc43a1e63838ea901ada1b4
Tianjian Wang <wangtjwork@gmail.com>
2024-07-14 11:50:02 -0700

    Convert into progressive web app with a new logo fron Canva

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "3_mins_musical_societies",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: '基础乐理练习', // <--- 👀
+        short_name: '乐理练习', // <--- 👀
+        description: '一个小巧的App来帮助你随时随地练习乐理', // <--- 👀
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
