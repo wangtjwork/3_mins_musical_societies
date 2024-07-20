589b73713f78ee74990c20675111939b803dd135
Tianjian Wang <wangtjwork@gmail.com>
2024-07-20 09:31:24 -0700

    KAN-2 Add appMode for basic routing support

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './App.css'
 import { generateSingleNoteXml, singleNoteXmlDoc } from './utils/musicXMLUtils';
-import { TREBLE_CLEF_NOTES_SCIENTIFIC } from './utils/musicNotesConfig';
+import { TREBLE_CLEF_NOTES_SCIENTIFIC } from './constants/musicNotesConfig';
 import SingleNoteSheet from './components/SingleNoteSheet';
-import { AppBar, Box, Button, CssBaseline, Stack, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, CssBaseline, IconButton, Stack, Toolbar, Typography } from '@mui/material';
 import SingleNotePickerForm from './components/SingleNotePickerForm';
-import { PlayArrow, Refresh } from '@mui/icons-material';
+import { ArrowBack, PlayArrow, Refresh } from '@mui/icons-material';
 import { playNote } from './utils/playSoundUtils';
+import { AppMode } from './constants/AppMode';
 
 function App() {
+  const [appMode, setAppMode] = useState<AppMode>(AppMode.INITIAL);
+
   const [note, setNote] = useState<string>('');
   const [xmlDoc, setXmlDoc] = useState<XMLDocument | null>(null);
 
   const onStartClick = () => {
+    setAppMode(AppMode.TEST_NOTE_TO_PITCH);
     const randomNote = TREBLE_CLEF_NOTES_SCIENTIFIC[Math.floor(Math.random() * TREBLE_CLEF_NOTES_SCIENTIFIC.length)];
     const randomNoteXmlDoc = generateSingleNoteXml(singleNoteXmlDoc, randomNote[0], randomNote[1]);
     setNote(randomNote);
@@ -33,6 +37,19 @@ function App() {
       <Box flexDirection={'column'} alignItems={'stretch'} textAlign={'center'} marginLeft={5} marginRight={5}>
         <AppBar position='static'>
           <Toolbar>
+            {
+              appMode != AppMode.INITIAL
+                ? (<IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: 2 }}
+                >
+                  <ArrowBack onClick={() => setAppMode(AppMode.INITIAL)} />
+                </IconButton>)
+                : null
+            }
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               五线谱读音名练习
             </Typography>
---
+++ src/constants/AppMode.ts
@@ -0,0 +1,4 @@
+export enum AppMode {
+    INITIAL,
+    TEST_NOTE_TO_PITCH
+};
