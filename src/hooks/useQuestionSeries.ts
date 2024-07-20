c537d06e9c39c9a5054c73fd10b4c70b09d86a3b
Tianjian Wang <wangtjwork@gmail.com>
2024-07-20 09:20:17 -0700

    KAN-2 Add hook for question index management

---
+++ src/hooks/useQuestionSeries.ts
@@ -0,0 +1,25 @@
+import { useCallback, useMemo, useState } from "react";
+
+type QuestionSeriesRet = {
+    index: number,
+    hasNext: boolean,
+    goToNext: () => void,
+    hasPrevious: boolean,
+    goToPrevious: () => void
+};
+
+function useQuestionSeries(seriesLength: number): QuestionSeriesRet {
+    const [index, setIndex] = useState(0);
+
+    const goToNext = useCallback(() => setIndex((i) => i + 1), [setIndex]);
+    const hasNext = useMemo(() => index < seriesLength, [index, seriesLength]);
+
+    const goToPrevious = useCallback(() => setIndex((i) => i + 1), [setIndex]);
+    const hasPrevious = useMemo(() => index > 0, [index, seriesLength]);
+
+    return {
+        index, hasNext, goToNext, hasPrevious, goToPrevious
+    };
+}
+
+export default useQuestionSeries;
