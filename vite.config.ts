bcb5337e771bd1671f048bc647a850c39efe1f65
Tianjian Wang <wangtjwork@gmail.com>
2024-08-11 12:27:42 -0700

    KAN-23 Remove base param from vite.config.ts
    The base param is only used for GitHub Pages deployment. If we switch to Vercel, we'll have 404 error.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "3_mins_musical_societies",
   plugins: [
     react(),
     VitePWA({
